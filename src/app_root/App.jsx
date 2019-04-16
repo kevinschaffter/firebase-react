04e8e4a54a2ca6564991a21073b3db6dc40e2071
kschaffter <kschaffter@carecloud.com>
2019-04-15 20:38:43 -0400

    Basic Setup

---
+++ src/app_root/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button icon="smile" content="Thing" />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import App from './app_root/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// potentially wrap everything here?
+
+const render = _ => ReactDOM.render(<App />, rootEl);
+
+// TO DO: maybe add a timeout?
+if (module.hot) module.hot.accept(App, _ => render());
+
+render();
+
 serviceWorker.unregister();
